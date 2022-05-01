656aaa8194acca0fc235bad9d266a4b98ffa29fa
Praveen1803 <54043737+Praveen1803@users.noreply.github.com>
2022-05-01 17:25:01 +0200

    Update webcam.py
    Added custom drawing utility

--- webcam.py
+++ webcam.py
@@ -6,16 +6,17 @@ import mediapipe as mp
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_pose = mp.solutions.pose
+landmarks= mp.solutions.pose.PoseLandmark
+
 
 cap = cv2.VideoCapture(0)
 with mp_pose.Pose(
     min_detection_confidence=0.8,
-    min_tracking_confidence=0.5, enable_segmentation=False) as pose:
+    min_tracking_confidence=0.8, enable_segmentation=False) as pose:
   while cap.isOpened():
     success, image = cap.read()
     if not success:
-      print("Ignoring empty camera frame.")
-      # If loading a video, use 'break' instead of 'continue'.
+      print("If reading input stream is unsucessful!!!")
       continue
       
 
@@ -24,7 +25,9 @@ with mp_pose.Pose(
     image.flags.writeable = False
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     results = pose.process(image)
-    
+    nose= results.pose_landmarks.landmark[0]
+    print(nose)
+
     # Draw the pose annotation on the image.
     image.flags.writeable = True
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
@@ -32,7 +35,8 @@ with mp_pose.Pose(
         image,
         results.pose_landmarks,
         mp_pose.POSE_CONNECTIONS,
-        landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
+        landmark_drawing_spec=mp_drawing.DrawingSpec(color=(255,255,255),thickness=3, circle_radius=3),
+        connection_drawing_spec=mp_drawing.DrawingSpec(color=(49,125,237),thickness=3, circle_radius=3))
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('MediaPipe Pose', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
